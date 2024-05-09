009ca1a1ec228f216323ea1b621de94a183fad5b
chante81 <chanteosborne@gmail.com>
2024-05-09 14:45:54 -0400

    latest

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26c2bde9e8a8e1bf9711f4fbf259cf0bc45cd165")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccd065817fa1e1640e4ee7762e0ca9a0435964e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
